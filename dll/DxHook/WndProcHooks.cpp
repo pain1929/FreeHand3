6aa820513a4b49dc91fa6f99f59e36cf0d589965
pain1929 <pain1929@163.com>
2025-12-16 23:17:44 +0800

    处理窗口问题

--- dll/DxHook/WndProcHooks.cpp
+++ dll/DxHook/WndProcHooks.cpp
@@ -27,7 +27,7 @@ static LRESULT hkWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
     ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
 
-    if (HotKeys::Menu::showMenu) {
+    if (MainMenu::showMenu) {
         switch (uMsg) {
         case WM_MOUSEMOVE:
         case WM_NCMOUSEMOVE:
